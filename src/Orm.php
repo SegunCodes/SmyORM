612e388e09e54fb2f6ec063288eac17f17127670
SegunCodes <shegstix64@gmail.com>
2023-05-19 16:52:45 +0200

    set up orm functions and composer json

---
+++ src/Orm.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace SmyORM\SmyORM;
+/**
+ * SmyORM - SmyORM is a PHP Object-relational mapping (ORM)
+ * @author SegunCodes
+*/ 
+class Orm{
+    private $db;
+
+    public function __construct(\PDO $db) {
+        $this->db = $db;
+    }
+
+    abstract public function tableName(): string;
+    abstract public function attributes(): array;
+
+    public function save(){
+        $tableName = $this->tableName();
+        $attributes = $this->attributes();
+        $params = array_map(fn($attr) => ":$attr", $attributes);
+        $statement = $this->prepare("INSERT INTO $tableName (".implode(',', $attributes).") 
+        VALUES (".implode(',', $params).")");
+        foreach ($attributes as $attribute) {
+            $statement->bindValue(":$attribute", $this->{$attribute});
+        }
+        $statement->execute();
+        return true;
+    }
+
+    public function findOne($where){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("SELECT * FROM $tableName WHERE $sql"); 
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        $stmt->execute();
+        return $stmt->fetchObject(static::class);
+    }
+
+    public function findOneOrWhere($where, $orWhere){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $orpart = array_keys($orWhere); 
+        $sql2 = implode(" AND ", array_map(fn($or) => "$or = :$or", $orpart));
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("SELECT * FROM $tableName WHERE $sql or $sql2"); 
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        foreach ($orWhere as $keys => $items) {
+            $stmt->bindValue(":$keys", $items);
+        }
+        $stmt->execute();
+        return $stmt->fetchObject(static::class);
+    }
+
+    public function findAll(){
+        $tableName = static::tableName();
+        $stmt = $this->prepare("SELECT * FROM $tableName ORDER BY id DESC");
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function findAllWhere($where){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("SELECT * FROM $tableName WHERE $sql ORDER BY id DESC");
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function findAllOrWhere($where, $orWhere){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $orpart = array_keys($orWhere); 
+        $sql2 = implode(" AND ", array_map(fn($or) => "$or = :$or", $orpart));
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("SELECT * FROM $tableName WHERE $sql or $sql2 ORDER BY id DESC");
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        foreach ($orWhere as $keys => $items) {
+            $stmt->bindValue(":$keys", $items);
+        }
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function count(){
+        $tableName = static::tableName();
+        $stmt = $this->prepare("SELECT count(*) FROM $tableName"); 
+        $stmt->execute();
+        return $stmt->fetchColumn();
+    }
+
+    public function countWhere($where){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("SELECT count(*) FROM $tableName WHERE $sql");
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        $stmt->execute();
+        return $stmt->fetchColumn();
+    }
+
+    public function countOrWhere($where, $orWhere){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $orpart = array_keys($orWhere); 
+        $sql2 = implode(" AND ", array_map(fn($or) => "$or = :$or", $orpart));
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("SELECT count(*) FROM $tableName WHERE $sql OR $sql2");
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        foreach ($orWhere as $keys => $items) {
+            $stmt->bindValue(":$keys", $items);
+        }
+        $stmt->execute();
+        return $stmt->fetchColumn();
+    }
+
+    public function delete($where){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("DELETE FROM $tableName WHERE $sql");
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        return $stmt->execute();
+    }
+
+    public function deleteOrWhere($where, $orWhere){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $orpart = array_keys($orWhere); 
+        $sql2 = implode(" AND ", array_map(fn($or) => "$or = :$or", $orpart));
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("DELETE FROM $tableName WHERE $sql OR $sql2");
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        foreach ($orWhere as $keys => $items) {
+            $stmt->bindValue(":$keys", $items);
+        }
+        return $stmt->execute();
+    }
+
+    public function update($data, $where){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $set = array_keys($data);
+        $setData = implode(", ", array_map(fn($d) => "$d = :$d", $set));
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("UPDATE $tableName SET $setData WHERE $sql");
+        foreach ($data as $keys => $items) {
+            $stmt->bindValue(":$keys", $items);
+        }
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        return $stmt->execute();
+    }
+
+    public function updateOrWhere($data, $where, $orWhere){
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $set = array_keys($data);
+        $orpart = array_keys($orWhere); 
+        $sql2 = implode(" AND ", array_map(fn($or) => "$or = :$or", $orpart));
+        $setData = implode(", ", array_map(fn($d) => "$d = :$d", $set));
+        $sql = implode(" AND ", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $stmt = $this->prepare("UPDATE $tableName SET $setData WHERE $sql or $sql2");
+        foreach ($data as $keys => $items) {
+            $stmt->bindValue(":$keys", $items);
+        }
+        foreach ($where as $key => $item) {
+            $stmt->bindValue(":$key", $item);
+        }
+        foreach ($orWhere as $keys => $items) {
+            $stmt->bindValue(":$keys", $items);
+        }
+        return $stmt->execute();
+    }
+
+    public function prepare($sql){
+        return $this->db->prepare($sql);
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'seguncodes/smyorm',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5f1394ba072f98cb5dd0f380168992241134eaef',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'seguncodes/smyorm' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5f1394ba072f98cb5dd0f380168992241134eaef',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
